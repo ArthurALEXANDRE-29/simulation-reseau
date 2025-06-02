cf267ec9cdd8958315885fb19d503ea9bc5ebbc7
Andy PHAN <andy.phan@etu.unistra.fr>
2025-06-02 16:30:34 +0200

    ezfnezjfvnlezvnezkjvbevbevkjbvkjebvkejzfzejl

--- src/trame.h
+++ src/trame.h
@@ -16,17 +16,7 @@ typedef struct {
     uint32_t fcs;
 } trame;
 
-// Types de trames Ethernet
-#define TYPE_IPV4 0x0800
-#define TYPE_ARP  0x0806
-#define TYPE_IPV6 0x86DD
-#define TYPE_STP  0x0027
 
 bool init_trame(trame *t, MAC src, MAC dest, uint16_t type, const uint8_t *donnees, size_t taille_donnees);
 void deinit_trame(trame *t);
 void afficher_trame(const trame *t);
-void afficher_trame_utilisateur(const trame *t);
-void afficher_trame_hexa(const trame *t);
-void calculer_fcs(trame *t);
-bool verifier_trame(const trame *t);
-trame dupliquer_trame(const trame *t);
