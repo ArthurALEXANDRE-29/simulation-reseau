3be35bafe9eccb6576e967aad108ed40dd69cc46
Arthur ALEXANDRE <arthur.alexandre682006@gmail.com>
2025-06-12 14:03:51 +0200

    Update stp.h and switch structures

--- src/stp.h
+++ src/stp.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "adresse.h"
-#include "switch.h"
+#include "switch.h" 
 #include "graphe.h"
 
+// Structure pour les BPDU
 typedef struct {
     mac_addr_t root_mac;
     int root_priority;
@@ -11,6 +12,7 @@ typedef struct {
     mac_addr_t sender_mac;
 } bpdu_t;
 
+// Structure pour les informations STP du switch
 typedef struct {
     mac_addr_t root_mac;
     int root_priority;
@@ -18,6 +20,34 @@ typedef struct {
     int root_port;
 } switch_stp_t;
 
+// Structure étendue pour les informations STP des ports
+typedef struct {
+    bpdu_t last_bpdu;
+    bool received_bpdu;
+    // Les autres infos (state, role, cost) sont dans port_info_t du switch
+} port_stp_extended_t;
+
+// Fonctions de base
 void init_stp(switch_stp_t *stp, switch_t *sw);
 void deinit_stp(switch_stp_t *stp);
+
+// Fonctions utilitaires
+int compare_mac(const mac_addr_t *mac1, const mac_addr_t *mac2);
+bool is_better_bpdu(const bpdu_t *bpdu1, const bpdu_t *bpdu2);
+bpdu_t create_bpdu(switch_stp_t *stp, switch_t *sw, int port_cost);
+
+// Fonctions de traitement STP
+void send_bpdu(switch_t *sw, int port, bpdu_t *bpdu, graphe *g);
+bool process_received_bpdu(switch_stp_t *stp, switch_t *sw, bpdu_t *received_bpdu, 
+                          int receiving_port, int port_cost);
+void determine_port_roles(switch_stp_t *stp, switch_t *sw, graphe *g, 
+                         port_stp_extended_t *port_ext_info, int *port_costs);
+void update_port_states(switch_t *switches, int nb_switches);
+
+// Fonction principale et affichage
+void run_stp_algorithm(graphe *g, switch_t *switches, int nb_switches, int *port_costs);
+void print_stp_state(switch_stp_t *stp_info, switch_t *switches, 
+                     port_stp_extended_t **port_ext_infos, int nb_switches);
+
+// Dijkstra
 void dijkstra(graphe const *g, sommet s, int const *poids_arete, int *distance_sommet);
--- src/switch.h
+++ src/switch.h
@@ -16,17 +16,30 @@ typedef struct {
     int capacite;
 } table_commutation_t;
 
-// Énumération pour l'état des ports STP
+// États STP des ports (conformes au standard IEEE 802.1D)
 typedef enum {
-    PORT_INCONNU = 0,
-    PORT_RACINE,
-    PORT_DESIGNE,
-    PORT_BLOQUE
-} etat_port_t;
+    PORT_DISABLED = 0,
+    PORT_BLOCKING,
+    PORT_LISTENING,
+    PORT_LEARNING,
+    PORT_FORWARDING
+} port_state_t;
 
+// Rôles STP des ports
+typedef enum {
+    PORT_UNKNOWN = 0,
+    PORT_ROOT,
+    PORT_DESIGNATED,
+    PORT_BLOCKED
+} port_role_t;
+
+// Informations complètes d'un port
 typedef struct {
-    etat_port_t etat;
+    port_state_t state;
+    port_role_t role;
     bool actif;
+    int cost;
+    // Autres informations STP si nécessaire
 } port_info_t;
 
 typedef struct {
@@ -37,6 +50,7 @@ typedef struct {
     port_info_t *ports;
 } switch_t;
 
+// Fonctions de table de commutation
 void init_table_commutation(table_commutation_t *table);
 void deinit_table_commutation(table_commutation_t *table);
 bool ajouter_entree_table(table_commutation_t *table, mac_addr_t mac, int port);
@@ -44,17 +58,23 @@ int chercher_port_mac(const table_commutation_t *table, mac_addr_t mac);
 void vider_table_commutation(table_commutation_t *table);
 void afficher_table_commutation(const table_commutation_t *table);
 
+// Fonctions de switch
 void init_switch(switch_t *sw);
 void deinit_switch(switch_t *sw);
 switch_t creer_switch(mac_addr_t mac, int nb_ports, int priorite);
 void afficher_switch(const switch_t *sw);
 
+// Fonctions de gestion des ports
 void init_ports(switch_t *sw);
-void set_etat_port(switch_t *sw, int port, etat_port_t etat);
-etat_port_t get_etat_port(const switch_t *sw, int port);
+void set_port_state(switch_t *sw, int port, port_state_t state);
+port_state_t get_port_state(const switch_t *sw, int port);
+void set_port_role(switch_t *sw, int port, port_role_t role);
+port_role_t get_port_role(const switch_t *sw, int port);
 void activer_port(switch_t *sw, int port);
 void desactiver_port(switch_t *sw, int port);
 bool port_est_actif(const switch_t *sw, int port);
 
+// Fonctions utilitaires
 bool switch_equals(const switch_t *s1, const switch_t *s2);
-const char* etat_port_to_string(etat_port_t etat);
+const char* port_state_to_string(port_state_t state);
+const char* port_role_to_string(port_role_t role);
