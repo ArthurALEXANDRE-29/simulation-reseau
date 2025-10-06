c7c05264e40f2e5b7ce4e90abe965fc7f7094d64
Arthur ALEXANDRE <arthur.alexandre682006@gmail.com>
2025-10-06 23:52:14 +0200

    Add IPv4 type and update related functions

--- src/adresse.h
+++ src/adresse.h
@@ -13,6 +13,7 @@ typedef struct {
 } ip_addr_t;
 
 typedef mac_addr_t MAC;
+typedef ip_addr_t IPv4;
 
 void init_mac(mac_addr_t *mac);
 void init_ip(ip_addr_t *ip);
@@ -23,12 +24,9 @@ ip_addr_t creer_ip(uint8_t o1, uint8_t o2, uint8_t o3, uint8_t o4);
 void afficher_mac(mac_addr_t mac);
 void afficher_ip(ip_addr_t ip);
 
-HEAD
-char* mac_to_string(mac_addr_t mac, char *str_mac);
-char* ip_to_string(ip_addr_t ip, char *str_ip);
-void init_IPv4(IPv4 *adresse);
-void init_MAC(IPv4 *adresse);
-char * MAC_to_string(MAC M, char * str_mac);
+void init_IPv4(ip_addr_t *adresse);
+void init_MAC(mac_addr_t *adresse);
+char * MAC_to_string(mac_addr_t M, char * str_mac);
 
 bool parse_mac(const char *str, mac_addr_t *mac);
 bool parse_ip(const char *str, ip_addr_t *ip);
